2b091a5865ab486f3b9d072850e067913c4041b3
ndinu <ndinu@users.noreply.github.com>
2018-07-27 15:18:06 -0300

    MULE-15445: When SFTP listeners are configured to move to a directory and auto-delete deletes files but doesn't move them (#47)

--- src/main/java/org/mule/extension/sftp/internal/source/SftpDirectoryListener.java
+++ src/main/java/org/mule/extension/sftp/internal/source/SftpDirectoryListener.java
@@ -11,6 +11,7 @@ import static org.mule.extension.file.common.api.FileDisplayConstants.MATCHER;
 import static org.mule.runtime.core.api.util.IOUtils.closeQuietly;
 import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
 import static org.mule.runtime.extension.api.runtime.source.PollContext.PollItemStatus.SOURCE_STOPPING;
+
 import org.mule.extension.file.common.api.matcher.NullFilePayloadPredicate;
 import org.mule.extension.sftp.api.SftpFileAttributes;
 import org.mule.extension.sftp.api.SftpFileMatcher;
@@ -278,21 +279,11 @@ public class SftpDirectoryListener extends PollingSource<InputStream, SftpFileAt
   }
 
   private void postAction(PostActionGroup postAction, SourceCallbackContext ctx) {
-    try {
-      postAction.validateSelf();
-    } catch (IllegalArgumentException e) {
-      LOGGER.error(e.getMessage());
-    }
 
     SftpFileSystem fileSystem = ctx.getConnection();
     fileSystem.changeToBaseDir();
     ctx.<SftpFileAttributes>getVariable(ATTRIBUTES_CONTEXT_VAR).ifPresent(attrs -> {
-      if (postAction.isAutoDelete()) {
-        fileSystem.delete(attrs.getPath());
-      } else if (postAction.getMoveToDirectory() != null) {
-        fileSystem.move(config, attrs.getPath(), postAction.getMoveToDirectory(), false, true,
-                        postAction.getRenameTo());
-      }
+      postAction.apply(fileSystem, attrs, config);
     });
   }
 
