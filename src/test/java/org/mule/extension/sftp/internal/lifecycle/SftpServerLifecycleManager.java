777a35e1919c1e589b7335e76fcb040c70e117e7
ndinu <ndinu@users.noreply.github.com>
2019-08-12 19:27:15 -0300

    MULE-17317:  Add MTF tests to SFTP Connector (#70)

---
+++ src/test/java/org/mule/extension/sftp/internal/lifecycle/SftpServerLifecycleManager.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.sftp.internal.lifecycle;
+
+import static org.mule.test.extension.file.common.api.FileTestHarness.WORKING_DIR_SYSTEM_PROPERTY;
+
+import org.mule.extension.sftp.SftpServer;
+
+import java.io.IOException;
+
+import org.junit.rules.TemporaryFolder;
+
+public class SftpServerLifecycleManager {
+
+
+  private static TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private static SftpServer sftpServer;
+
+  public enum AuthType {
+    USER_PASSWORD, PUBLIC_KEY
+  }
+
+  public static void startSftpServer(String port) throws Exception {
+    createAndStartServer(Integer.valueOf(port));
+  }
+
+  private static void createAndStartServer(Integer port) throws IOException {
+    temporaryFolder.create();
+    setUpServer(port);
+  }
+
+  private static void setUpServer(int port) {
+    sftpServer = new SftpServer(port, temporaryFolder.getRoot().toPath());
+    sftpServer.setPasswordAuthenticator();
+    sftpServer.start();
+  }
+
+  public static void stopSftpServer() throws Exception {
+    try {
+      if (sftpServer != null) {
+        sftpServer.stop();
+      }
+    } finally {
+      temporaryFolder.delete();
+      System.clearProperty(WORKING_DIR_SYSTEM_PROPERTY);
+    }
+  }
+
+}
