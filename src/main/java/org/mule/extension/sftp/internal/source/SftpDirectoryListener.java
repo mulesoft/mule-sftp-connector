4b3df00402cda2113e717133ed5d83c64ff94355
SebaElizalde <sebi.elizalde@gmail.com>
2019-09-24 17:43:33 -0300

    curly logger

--- src/main/java/org/mule/extension/sftp/internal/source/SftpDirectoryListener.java
+++ src/main/java/org/mule/extension/sftp/internal/source/SftpDirectoryListener.java
@@ -234,7 +234,7 @@ public class SftpDirectoryListener extends PollingSource<InputStream, SftpFileAt
     try {
       fileSystem.changeToBaseDir();
     } catch (Exception e) {
-      LOGGER.debug("Exception while trying to open connection. Cause: " + e.getCause() + ". Message: " + e.getMessage());
+      LOGGER.debug("Exception while trying to open connection. Cause: {} . Message: {}", e.getCause(), e.getMessage());
       fileSystemProvider.disconnect(fileSystem);
       throw e;
     }
