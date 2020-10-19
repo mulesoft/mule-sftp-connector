c800abcc240c1da5d97978e216837b1488a0459d
JohnnyBendik <jan.bendik@hotovo.org>
2020-10-19 17:14:35 +0200

    Added support for sample data

---
+++ src/main/java/org/mule/extension/sftp/internal/sampledata/ObjectSampleDataProvider.java
@@ -0,0 +1,17 @@
+package org.mule.extension.sftp.internal.sampledata;
+
+import org.mule.sdk.api.data.sample.SampleDataException;
+import org.mule.sdk.api.data.sample.SampleDataProvider;
+import org.mule.sdk.api.runtime.operation.Result;
+
+public class ObjectSampleDataProvider implements SampleDataProvider {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public Result getSample() throws SampleDataException {
+        return null;
+    }
+}
