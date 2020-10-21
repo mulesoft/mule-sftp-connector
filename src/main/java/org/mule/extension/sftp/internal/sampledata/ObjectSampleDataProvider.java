bcd0d2c79176a3828535e79476419124e47adb6b
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-10-21 13:10:47 -0300

    wrong dependencies in pom

--- src/main/java/org/mule/extension/sftp/internal/sampledata/ObjectSampleDataProvider.java
+++ src/main/java/org/mule/extension/sftp/internal/sampledata/ObjectSampleDataProvider.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.extension.sftp.internal.sampledata;
 
 import org.mule.extension.file.common.api.BaseFileSystemOperations;
@@ -16,31 +22,31 @@ import java.util.List;
 
 public class ObjectSampleDataProvider extends BaseFileSystemOperations implements SampleDataProvider {
 
-    @Parameter
-    String directoryPath;
+  @Parameter
+  String directoryPath;
 
-    @Config
-    FileConnectorConfig config;
+  @Config
+  FileConnectorConfig config;
 
-    @Connection
-    FileSystem connection;
+  @Connection
+  FileSystem connection;
 
 
-    @Override
-    public String getId() {
-        return getClass().getSimpleName();
+  @Override
+  public String getId() {
+    return getClass().getSimpleName();
+  }
+
+  @Override
+  public Result<InputStream, FileAttributes> getSample() throws SampleDataException {
+
+    connection.changeToBaseDir();
+    List<Result<InputStream, FileAttributes>> files = connection.list(config, directoryPath, false, null, null);
+
+    if (files.isEmpty()) {
+      throw new SampleDataException("No data available", SampleDataException.NO_DATA_AVAILABLE);
     }
 
-    @Override
-    public Result<InputStream, FileAttributes> getSample() throws SampleDataException {
-
-        connection.changeToBaseDir();
-        List<Result<InputStream, FileAttributes>> files = connection.list(config, directoryPath, false, null, null);
-
-        if (files.isEmpty()){
-            throw new SampleDataException("No data available", SampleDataException.NO_DATA_AVAILABLE);
-        }
-
-        return files.get(0);
-    }
+    return files.get(0);
+  }
 }
