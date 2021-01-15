984eb2c0bfdda157bb5a4e272032e59a0eba34de
emariotti3 <eugeniamariotti93@gmail.com>
2021-01-15 12:45:52 -0300

    First attempt at testing sftp reconnection using TITA.

--- src/test/java/org/mule/extension/sftp/SftpDirectoryListenerReconnectionTestCase.java
+++ src/test/java/org/mule/extension/sftp/SftpDirectoryListenerReconnectionTestCase.java
@@ -1,28 +1,77 @@
 /*
- * (c) 2003-2020 MuleSoft, Inc. This software is protected under international copyright
- * law. All use of this software is subject to MuleSoft's Master Subscription Agreement
- * (or other master license agreement) separately entered into in writing between you and
- * MuleSoft. If such an agreement is not in place, you may not use the software.
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 package org.mule.extension.sftp;
 
+import static com.mulesoft.anypoint.tita.environment.api.artifact.Identifier.identifier;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import com.mulesoft.anypoint.tests.http.HttpResponse;
 import com.mulesoft.anypoint.tita.environment.api.ApplicationSelector;
+import com.mulesoft.anypoint.tita.environment.api.anypoint.ApiManager;
 import com.mulesoft.anypoint.tita.environment.api.artifact.ApplicationBuilder;
+import com.mulesoft.anypoint.tita.environment.api.artifact.Identifier;
+import com.mulesoft.anypoint.tita.environment.api.artifact.policy.PolicySupplier;
+import com.mulesoft.anypoint.tita.environment.api.runtime.Runtime;
 import com.mulesoft.anypoint.tita.runner.ambar.Ambar;
 import com.mulesoft.anypoint.tita.runner.ambar.annotation.Application;
+import com.mulesoft.anypoint.tita.runner.ambar.annotation.Platform;
+import com.mulesoft.anypoint.tita.runner.ambar.annotation.Policy;
+import com.mulesoft.anypoint.tita.runner.ambar.annotation.TestTarget;
 import com.mulesoft.anypoint.tita.runner.ambar.annotation.runtime.Standalone;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.apache.maven.model.Dependency;
 
 @RunWith(Ambar.class)
 public class SftpDirectoryListenerReconnectionTestCase {
 
-    @Standalone
-    Runtime runtime;
+  private static final Identifier api = identifier("api1");
+  private static final Identifier port = identifier("port");
 
-    @Application
-    public static ApplicationBuilder app(ApplicationSelector runtimeBuilder) {
-        return runtimeBuilder.custom("sftp-reconnection-app", "sftp-reconnection-app.xml");
-    }
+  //TODO: usar atributo 'testing' de Standalone para especificar la version del runtime
+  @Standalone
+  Runtime runtime;
+
+  @Before
+  public void setup() {
+    //Las opciones son ver si se resuelve el puerto en el before o agregar un processor
+    //a la chain de processors de Ambar(ahi puedo agregar lo que quiera, incluso levantar
+    //un docker ahi programaticamente, o algo asi).
+  }
+
+  @Application
+  public static ApplicationBuilder app(ApplicationSelector runtimeBuilder) {
+    return runtimeBuilder
+        .custom("sftp-reconnection-app", "sftp-reconnection-app.xml")
+        .withDependency(sftpConnectorDependency())
+        .withProperty("sftp.port", "2222") //.withProperty("sftp.port", System.getProperty("sftp.listener.port"))
+        .withApi(api, port);
+  }
 
 
+
+  @Test
+  public void sftpReconnectionTestCase() {
+    HttpResponse responseApi1 = runtime.api(api).request("/stop").withPayload("My payload").get();
+
+    assertThat(responseApi1.statusCode(), is(200));
+
+  }
+
+
+  private static Dependency sftpConnectorDependency() {
+    Dependency sftpConnector = new Dependency();
+    sftpConnector.setGroupId("org.mule.connectors");
+    sftpConnector.setArtifactId("mule-sftp-connector");
+    sftpConnector.setVersion("1.3.8");
+    sftpConnector.setClassifier("mule-plugin");
+
+    return sftpConnector;
+  }
 }
--- src/test/java/org/mule/extension/sftp/internal/lifecycle/SftpServerContainerLifecycleManger.java
+++ src/test/java/org/mule/extension/sftp/internal/lifecycle/SftpServerContainerLifecycleManger.java
@@ -1,4 +1,78 @@
-package org.mule.extension.sftp;
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.sftp.internal.lifecycle;
+
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.messages.Container;
+import static com.spotify.docker.client.DockerClient.ListContainersParam.allContainers;
+
+import java.util.List;
+
+import static java.lang.String.format;
 
 public class SftpServerContainerLifecycleManger {
+
+  //private static final String DOCKER_IMAGE = "linuxserver/openssh-server:latest";
+  private static final String SFTP_SERVER_NAME = "openssh";
+
+  public static Container getContainerByName(String containerName) throws Exception {
+    final DockerClient docker = DefaultDockerClient.fromEnv().build();
+    final List<Container> containers = docker.listContainers();
+
+    for (Container container : containers) {
+      if (container.image().contains(containerName)) {
+        return container;
+      }
+    }
+
+    throw new Exception(format("No container found for name {}", containerName));
+
+    //final ContainerConfig config = ContainerConfig.builder()
+    //        .image(DOCKER_IMAGE)
+    //        .build();
+
+    //final ContainerCreation creation = docker.createContainer(config, IMAGE_NAME);
+
+  }
+
+  public static Container getContainerByImageId(String imageId) throws Exception {
+    final DockerClient docker = DefaultDockerClient.fromEnv().build();
+    final List<Container> containers = docker.listContainers(allContainers());
+
+    for (Container container : containers) {
+      if (container.id().equals(imageId)) {
+        return container;
+      }
+    }
+
+    throw new Exception(format("No container found for id {}", imageId));
+
+    //final ContainerConfig config = ContainerConfig.builder()
+    //        .image(DOCKER_IMAGE)
+    //        .build();
+
+    //final ContainerCreation creation = docker.createContainer(config, IMAGE_NAME);
+
+  }
+
+  public static String stopServerContainer(String containerName, int delay) throws Exception {
+    final DockerClient docker = DefaultDockerClient.fromEnv().build();
+    Container sftpServerContainer = getContainerByName(SFTP_SERVER_NAME);
+    docker.stopContainer(sftpServerContainer.id(), delay);
+    return sftpServerContainer.id();
+  }
+
+  public static void startServerContainer(String containerId) throws Exception {
+    final DockerClient docker = DefaultDockerClient.fromEnv().build();
+    Container sftpServerContainer = getContainerByImageId(containerId);
+    //if (sftpServerContainer.state().equalsIgnoreCase("stopped")) {
+    docker.startContainer(containerId);
+    //}
+  }
+
 }
