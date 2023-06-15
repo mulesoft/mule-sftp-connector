19f1d10d8fe657043779b957074a02b87fc46183
Federico Burman <fburman@mulesoft.com>
2023-06-15 11:21:13 -0300

    Remove unused script

--- src/test/resources/setup-sftp-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CONTAINER_ID=$(docker ps -qf "name=openssh")
-docker exec $CONTAINER_ID mkdir /config/privateDirectory
-docker exec $CONTAINER_ID touch /config/privateDirectory/file-to-renamed.txt
-docker exec $CONTAINER_ID sudo chown -R root:root /config/privateDirectory
