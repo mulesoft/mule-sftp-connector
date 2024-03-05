46749ae6092c5f892fa18023bf6b2590d3fae415
VTorrazza <114665119+VTorrazza@users.noreply.github.com>
2024-03-05 15:47:31 -0300

    W-14875716 (#421)
    * upgrasing apache mina version
    
    * adding KEX feature
    
    * removing unused code
    
    * changing name
    
    * munit tests
    
    * removing parametrization
    
    * format
    
    * test correction

---
+++ src/main/java/org/mule/extension/sftp/internal/connection/NoStrictKexSession.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.sftp.internal.connection;
+
+import org.apache.sshd.client.ClientFactoryManager;
+import org.apache.sshd.client.session.ClientSessionImpl;
+import org.apache.sshd.common.io.IoSession;
+import org.apache.sshd.common.kex.KexProposalOption;
+import java.util.Map;
+
+class NoStrictKexSession extends ClientSessionImpl {
+
+  NoStrictKexSession(ClientFactoryManager client, IoSession ioSession) throws Exception {
+    super(client, ioSession);
+  }
+
+  @Override
+  protected Map<KexProposalOption, String> doStrictKexProposal(Map<KexProposalOption, String> proposal) {
+    return proposal;
+  }
+}
--- src/test/java/org/mule/extension/sftp/SftpTestHarness.java
+++ src/test/java/org/mule/extension/sftp/SftpTestHarness.java
@@ -121,7 +121,8 @@ public class SftpTestHarness extends AbstractSftpTestHarness {
 
   private SftpClient createDefaultSftpClient() throws IOException, GeneralSecurityException {
     SftpClient sftpClient =
-        new SftpClientFactory().createInstance("localhost", sftpPort.getNumber(), PRNGAlgorithm.SHA1PRNG, schedulerService, null);
+        new SftpClientFactory().createInstance("localhost", sftpPort.getNumber(), PRNGAlgorithm.SHA1PRNG, schedulerService, null,
+                                               true);
     clientAuthConfigurator.configure(sftpClient);
 
     sftpClient.setPassword(PASSWORD);
