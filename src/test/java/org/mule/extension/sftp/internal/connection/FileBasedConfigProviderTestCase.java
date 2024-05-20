7279f432673507ef8d9e6009b1b202a6c81e9932
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-05-20 17:18:15 +0530

    W-14557165: Added the required junit for ConfigFileReader

---
+++ src/test/java/org/mule/extension/sftp/internal/connection/FileBasedConfigProviderTestCase.java
@@ -0,0 +1,27 @@
+package org.mule.extension.sftp.internal.connection;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileBasedConfigProviderTestCase {
+
+    @Test
+    public void testFetchConfigFileProperties() {
+        FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("src/test/resources/mule_sshd_config");
+        Properties properties = fileBasedConfigProvider.getConfigProperties();
+        assertEquals(1, properties.size());
+        assertEquals("diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha512@ssh.com,diffie-hellman-group1-sha1",
+                properties.getProperty("KexAlgorithms"));
+    }
+
+    @Test
+    public void testFileNotFound() {
+        FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("mule_sshd");
+        Properties properties = fileBasedConfigProvider.getConfigProperties();
+        assertEquals(0, properties.size());
+    }
+
+}
