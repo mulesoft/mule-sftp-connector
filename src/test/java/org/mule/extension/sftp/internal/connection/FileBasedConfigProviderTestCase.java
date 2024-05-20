4407bd50128541b82969e0c90c80693573387e02
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-05-20 18:36:14 +0530

    W-14557165: Formatted

--- src/test/java/org/mule/extension/sftp/internal/connection/FileBasedConfigProviderTestCase.java
+++ src/test/java/org/mule/extension/sftp/internal/connection/FileBasedConfigProviderTestCase.java
@@ -8,20 +8,20 @@ import static org.junit.Assert.assertEquals;
 
 public class FileBasedConfigProviderTestCase {
 
-    @Test
-    public void testFetchConfigFileProperties() {
-        FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("src/test/resources/mule_sshd_config");
-        Properties properties = fileBasedConfigProvider.getConfigProperties();
-        assertEquals(1, properties.size());
-        assertEquals("diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha512@ssh.com,diffie-hellman-group1-sha1",
-                properties.getProperty("KexAlgorithms"));
-    }
+  @Test
+  public void testFetchConfigFileProperties() {
+    FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("src/test/resources/mule_sshd_config");
+    Properties properties = fileBasedConfigProvider.getConfigProperties();
+    assertEquals(1, properties.size());
+    assertEquals("diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha512@ssh.com,diffie-hellman-group1-sha1",
+                 properties.getProperty("KexAlgorithms"));
+  }
 
-    @Test
-    public void testFileNotFound() {
-        FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("mule_sshd");
-        Properties properties = fileBasedConfigProvider.getConfigProperties();
-        assertEquals(0, properties.size());
-    }
+  @Test
+  public void testFileNotFound() {
+    FileBasedConfigProvider fileBasedConfigProvider = new FileBasedConfigProvider("mule_sshd");
+    Properties properties = fileBasedConfigProvider.getConfigProperties();
+    assertEquals(0, properties.size());
+  }
 
 }
