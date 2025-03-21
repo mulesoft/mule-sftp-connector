d81a6b49b0dd828d426703ccdbbd7af2b08ced04
neetya26 <npasari@salesforce.com>
2025-03-21 16:16:57 +0530

    fixing coverage

---
+++ src/test/java/org/mule/extension/sftp/SftpConnectionSettingsTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.sftp;
+
+import org.junit.Test;
+import org.mule.extension.sftp.api.random.alg.PRNGAlgorithm;
+import org.mule.extension.sftp.internal.connection.AbstractExternalFileSystem;
+import org.mule.extension.sftp.internal.connection.AbstractFileSystem;
+import org.mule.extension.sftp.internal.connection.SftpConnectionSettings;
+import org.mule.tck.size.SmallTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@SmallTest
+public class SftpConnectionSettingsTestCase {
+
+  private SftpConnectionSettings settings = new SftpConnectionSettings();
+  private SftpConnectionSettings settings2 = new SftpConnectionSettings();
+
+
+  @Test
+  public void testSftpConnectionSettings() {
+    settings.setHost("localhost");
+    settings.setPort(22);
+    settings.setUsername("user");
+    settings.setPassword("pass");
+    settings.setPassphrase("phrase");
+    settings.setIdentityFile("/path/to/identity");
+    settings.setPrngAlgorithm(PRNGAlgorithm.SHA1PRNG);
+    settings.setKexHeader(true);
+
+
+    settings2.setHost("localhost");
+    settings2.setPort(22);
+    settings2.setUsername("user");
+    settings2.setPassword("pass");
+    settings2.setPassphrase("phrase");
+    settings2.setIdentityFile("/path/to/identity");
+    settings2.setPrngAlgorithm(PRNGAlgorithm.SHA1PRNG);
+    settings2.setKexHeader(true);
+
+    assertEquals(settings, settings2);
+
+    Object other = new Object();
+    assertNotEquals(settings, other);
+    settings.equals(settings);
+
+    settings.hashCode();
+  }
+}
