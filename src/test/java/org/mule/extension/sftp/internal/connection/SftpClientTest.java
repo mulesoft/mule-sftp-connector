ce7d165800aa3726423a003e92097146e2bdba88
neetya26 <npasari@salesforce.com>
2025-04-14 16:48:51 +0530

    added tests for UriUtilsTestCase and SftpClientTest

--- src/test/java/org/mule/extension/sftp/internal/connection/SftpClientTest.java
+++ src/test/java/org/mule/extension/sftp/internal/connection/SftpClientTest.java
@@ -9,9 +9,13 @@ package org.mule.extension.sftp.internal.connection;
 import org.junit.jupiter.api.Test;
 import org.mule.extension.sftp.api.SftpProxyConfig;
 import org.mule.extension.sftp.internal.exception.SftpConnectionException;
+import org.mule.extension.sftp.internal.lock.URLPathLock;
 import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.tck.size.SmallTest;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -19,11 +23,31 @@ import static org.mockito.Mockito.*;
 @SmallTest
 public class SftpClientTest {
 
+  private SftpClient client = mock(SftpClient.class);
+
   @Test
   public void testSftpConnectionException() throws ConnectionException {
-    SftpClient client = mock(SftpClient.class);
     doCallRealMethod().when(client).setProxyConfig(any());
     assertThrows(SftpConnectionException.class, () -> client.setProxyConfig(new SftpProxyConfig()));
+  }
+
+  @Test
+  public void testSftpClientGetAttributesNull() throws ConnectionException, IOException {
+    doCallRealMethod().when(client).getAttributes(any());
+    assertNull(client.getAttributes(null));
+  }
+
+  @Test
+  public void testSftpClientGetProxyConfigUsernameNull() throws ConnectionException, IOException {
+    SftpProxyConfig proxyConfig = new SftpProxyConfig();
+    proxyConfig.setHost("localhost");
+    proxyConfig.setPort(8081);
+    proxyConfig.setUsername("user");
+    doCallRealMethod().when(client).setProxyConfig(any());
+    assertThrows(SftpConnectionException.class, () -> client.setProxyConfig(proxyConfig));
+
+    URLPathLock urlPathLock = new URLPathLock(null, null);
+    urlPathLock.isLocked();
   }
 
 }
--- src/test/java/org/mule/extension/sftp/util/UriUtilsTestCase.java
+++ src/test/java/org/mule/extension/sftp/util/UriUtilsTestCase.java
@@ -10,24 +10,64 @@ import org.junit.jupiter.api.Test;
 import org.mule.extension.sftp.internal.exception.IllegalPathException;
 import org.mule.tck.size.SmallTest;
 
+import java.util.regex.PatternSyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mule.extension.sftp.internal.util.UriUtils.createUri;
+import static org.mule.extension.sftp.internal.util.UriUtils.toRegexPattern;
 
 @SmallTest
 public class UriUtilsTestCase {
 
-  private String basePathWithColon = ":Illegal path with colon";
-  private String basePathWithNewLine = "test\npath";
-  private String filePath = "";
-
   @Test
   public void testCreateUriWithURISyntaxException() {
-    assertThrows(IllegalPathException.class, () -> createUri(basePathWithColon, filePath));
+    String basePathWithColon = ":Illegal path with colon";
+    assertThrows(IllegalPathException.class, () -> createUri(basePathWithColon, ""));
   }
 
   @Test
   public void testCreateUriWithNewLine() {
-    assertThrows(IllegalPathException.class, () -> createUri(basePathWithNewLine, filePath));
+    String basePathWithNewLine = "test\npath";
+    assertThrows(IllegalPathException.class, () -> createUri(basePathWithNewLine, ""));
+  }
+
+  @Test
+  public void testCreateUriWithSeparator() {
+    String basePathWithSeparator = "path/";
+    assertEquals("path/path", createUri(basePathWithSeparator, "path").getPath());
+  }
+
+  @Test
+  public void testCreateUriWithBasePathLengthOne() {
+    String basePathWithSeparator = "p";
+    assertEquals("ppath", createUri(basePathWithSeparator, "path").getPath());
+  }
+
+  @Test
+  public void testCreateUriWithoutSeparator() {
+    String basePathWithSeparator = "path";
+    assertEquals("path/path", createUri(basePathWithSeparator, "path").getPath());
+  }
+
+  @Test
+  public void testRegexPatternWithCaret() {
+    assertThrows(PatternSyntaxException.class, () -> toRegexPattern("[^/"));
+  }
+
+  @Test
+  public void testRegexPatternWithCaretAndAmpersand() {
+    assertThrows(PatternSyntaxException.class, () -> toRegexPattern("[^&&"));
+  }
+
+  @Test
+  public void testRegexPatternWithCaretAndDash() {
+    assertThrows(PatternSyntaxException.class, () -> toRegexPattern("[^-"));
+  }
+
+  @Test
+  public void testRegexPatternWithCaretAndBracket() {
+    assertEquals("^[[^/]&&[\\^.]]$", toRegexPattern("[^.]"));
   }
 
 }
