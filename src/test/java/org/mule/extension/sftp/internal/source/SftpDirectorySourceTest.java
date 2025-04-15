b99b76c622ebb1703ddbfd4751e0be65145fd7be
neetya26 <npasari@salesforce.com>
2025-04-15 19:35:15 +0530

    cleaned tests and added URLPathLockTest, MuleSftpClientTest.

--- src/test/java/org/mule/extension/sftp/internal/source/SftpDirectorySourceTest.java
+++ src/test/java/org/mule/extension/sftp/internal/source/SftpDirectorySourceTest.java
@@ -8,26 +8,23 @@ package org.mule.extension.sftp.internal.source;
 
 import org.junit.jupiter.api.Test;
 import org.mule.extension.sftp.api.SftpFileAttributes;
-import org.mule.extension.sftp.internal.connection.FileSystem;
-import org.mule.extension.sftp.internal.connection.SftpFileSystemConnection;
-import org.mule.runtime.api.connection.ConnectionException;
-import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.source.PollContext;
-import org.mule.runtime.extension.api.runtime.source.PollingSource;
 import org.mule.tck.size.SmallTest;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SmallTest
 class SftpDirectorySourceTest {
 
+  private SftpDirectorySource sftpDirectorySource = spy(SftpDirectorySource.class);
+
   @Test
   public void testPollWithSourceStopping() {
-    SftpDirectorySource sftpDirectorySource = spy(SftpDirectorySource.class);
     PollContext context = mock(PollContext.class);
     when(context.isSourceStopping()).thenReturn(true);
     sftpDirectorySource.poll(context);
@@ -35,24 +32,18 @@ class SftpDirectorySourceTest {
 
   @Test
   public void testPollException() {
-
-    SftpDirectorySource sftpDirectorySource = spy(SftpDirectorySource.class);
     PollContext context = mock(PollContext.class);
-    //    ConnectionProvider provider = mock(ConnectionProvider.class);
-    //    when(provider.connect()).thenReturn(mock(SftpFileSystemConnection.class));
     when(context.isSourceStopping()).thenReturn(false);
     assertThrows(NullPointerException.class, () -> sftpDirectorySource.poll(context));
   }
 
-  //  @Test
-  //  public void testPollExceptionInOpenConnection() {
-  //    SftpFileSystemConnection mockConnection = mock(SftpFileSystemConnection.class);
-  //
-  //
-  //    SftpDirectorySource sftpDirectorySource = spy(SftpDirectorySource.class); doReturn(mockConnection).when(sftpDirectorySource).openConnection(any());
-  //    PollContext context = mock(PollContext.class);
-  //    when().thenReturn(mock(SftpFileSystemConnection.class));
-  //    when(context.isSourceStopping()).thenReturn(false);
-  //    assertThrows(NullPointerException.class, () -> sftpDirectorySource.poll(context));
-  //  }
+  @Test
+  public void testOnRejectedTime() throws IOException {
+    Result<InputStream, SftpFileAttributes> result = mock(Result.class);
+    InputStream mockStream = mock(InputStream.class);
+    when(result.getOutput()).thenReturn(mockStream);
+
+    sftpDirectorySource.onRejectedItem(result, null);
+    verify(mockStream, times(1)).close();
+  }
 }
