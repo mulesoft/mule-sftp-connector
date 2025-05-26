338b4de1a6456e594ac41219b687ec1e6e37b13a
sankarvsalesforce <sankarv@salesforce.com>
2025-05-26 16:40:58 +0530

    code smell for UriUtils

--- src/main/java/org/mule/extension/sftp/internal/util/UriUtils.java
+++ src/main/java/org/mule/extension/sftp/internal/util/UriUtils.java
@@ -130,137 +130,158 @@ public final class UriUtils {
   public static String toRegexPattern(String globPattern) {
     boolean inGroup = false;
     StringBuilder regex = new StringBuilder("^");
-
     int i = 0;
     while (i < globPattern.length()) {
       char c = globPattern.charAt(i++);
       switch (c) {
         case '\\':
-          // escape special characters
-          if (i == globPattern.length()) {
-            throw new PatternSyntaxException("No character to escape",
-                                             globPattern, i - 1);
-          }
-          char next = globPattern.charAt(i++);
-          if (isGlobMeta(next) || isRegexMeta(next)) {
-            regex.append('\\');
-          }
-          regex.append(next);
+          i = handleEscapeCharacter(globPattern, regex, i);
           break;
         case '/':
           regex.append(c);
           break;
         case '[':
-          // don't match name separator in class
-          regex.append("[[^/]&&[");
-          if (next(globPattern, i) == '^') {
-            // escape the regex negation char if it appears
-            regex.append("\\^");
-            i++;
-          } else {
-            // negation
-            if (next(globPattern, i) == '!') {
-              regex.append('^');
-              i++;
-            }
-            // hyphen allowed at start
-            if (next(globPattern, i) == '-') {
-              regex.append('-');
-              i++;
-            }
-          }
-          boolean hasRangeStart = false;
-          char last = 0;
-          while (i < globPattern.length()) {
-            c = globPattern.charAt(i++);
-            if (c == ']') {
-              break;
-            }
-            if (c == '/') {
-              throw new PatternSyntaxException("Explicit 'name separator' in class",
-                                               globPattern, i - 1);
-            }
-            // TBD: how to specify ']' in a class?
-            if (c == '\\' || c == '[' ||
-                c == '&' && next(globPattern, i) == '&') {
-              // escape '\', '[' or "&&" for regex class
-              regex.append('\\');
-            }
-            regex.append(c);
-
-            if (c == '-') {
-              if (!hasRangeStart) {
-                throw new PatternSyntaxException("Invalid range",
-                                                 globPattern, i - 1);
-              }
-              if ((c = next(globPattern, i++)) == EOL || c == ']') {
-                break;
-              }
-              if (c < last) {
-                throw new PatternSyntaxException("Invalid range",
-                                                 globPattern, i - 3);
-              }
-              regex.append(c);
-              hasRangeStart = false;
-            } else {
-              hasRangeStart = true;
-              last = c;
-            }
-          }
-          if (c != ']') {
-            throw new PatternSyntaxException("Missing ']", globPattern, i - 1);
-          }
-          regex.append("]]");
+          i = handleCharacterClass(globPattern, regex, i);
           break;
         case '{':
-          if (inGroup) {
-            throw new PatternSyntaxException("Cannot nest groups",
-                                             globPattern, i - 1);
-          }
-          regex.append("(?:(?:");
+          handleGroupStart(globPattern, regex, i, inGroup);
           inGroup = true;
           break;
         case '}':
-          if (inGroup) {
-            regex.append("))");
-            inGroup = false;
-          } else {
-            regex.append('}');
-          }
+          handleGroupEnd(regex, inGroup);
+          inGroup = false;
           break;
         case ',':
-          if (inGroup) {
-            regex.append(")|(?:");
-          } else {
-            regex.append(',');
-          }
+          handleComma(regex, inGroup);
           break;
         case '*':
-          if (next(globPattern, i) == '*') {
-            // crosses directory boundaries
-            regex.append(".*");
-            i++;
-          } else {
-            // within directory boundary
-            regex.append("[^/]*");
-          }
+          i = handleAsterisk(globPattern, regex, i);
           break;
         case '?':
           regex.append("[^/]");
           break;
         default:
-          if (isRegexMeta(c)) {
-            regex.append('\\');
-          }
-          regex.append(c);
+          handleDefaultCharacter(regex, c);
       }
     }
-
     if (inGroup) {
       throw new PatternSyntaxException("Missing '}", globPattern, i - 1);
     }
-
     return regex.append('$').toString();
+  }
+
+  private static void handleDefaultCharacter(StringBuilder regex, char c) {
+    if (isRegexMeta(c)) {
+      regex.append('\\');
+    }
+    regex.append(c);
+  }
+
+  private static int handleEscapeCharacter(String globPattern, StringBuilder regex, int i) {
+    if (i == globPattern.length()) {
+      throw new PatternSyntaxException("No character to escape", globPattern, i - 1);
+    }
+    char next = globPattern.charAt(i++);
+    if (isGlobMeta(next) || isRegexMeta(next)) {
+      regex.append('\\');
+    }
+    regex.append(next);
+    return i;
+  }
+  private static void handleGroupStart(String globPattern, StringBuilder regex, int i, boolean inGroup) {
+    if (inGroup) {
+      throw new PatternSyntaxException("Cannot nest groups", globPattern, i - 1);
+    }
+    regex.append("(?:(?:");
+  }
+  private static void handleGroupEnd(StringBuilder regex, boolean inGroup) {
+    if (inGroup) {
+      regex.append("))");
+    } else {
+      regex.append('}');
+    }
+  }
+  private static void handleComma(StringBuilder regex, boolean inGroup) {
+    if (inGroup) {
+      regex.append(")|(?:");
+    } else {
+      regex.append(',');
+    }
+  }
+  private static int handleAsterisk(String globPattern, StringBuilder regex, int i) {
+    if (next(globPattern, i) == '*') {
+      regex.append(".*");
+      i++;
+    } else {
+      regex.append("[^/]*");
+    }
+    return i;
+  }
+
+  private static int handleCharacterClass(String globPattern, StringBuilder regex, int startIndex) {
+    int i = startIndex;
+    regex.append("[[^/]&&[");
+    i = handleInitialCharacters(globPattern, regex, i);
+    boolean hasRangeStart = false;
+    char last = 0;
+    char c = '0';
+    while (i < globPattern.length()) {
+      c = globPattern.charAt(i++);
+      if (c == ']') {
+        break;
+      }
+      if (c == '/') {
+        throw new PatternSyntaxException("Explicit 'name separator' in class", globPattern, i - 1);
+      }
+      if (c == '\\' || c == '[' || (c == '&' && next(globPattern, i) == '&')) {
+        regex.append('\\');
+      }
+      regex.append(c);
+      if (c == '-') {
+        i = handleRange(globPattern, regex, i, hasRangeStart, last);
+        hasRangeStart = false;
+      } else {
+        hasRangeStart = true;
+        last = c;
+      }
+    }
+    if (c != ']') {
+      throw new PatternSyntaxException("Missing ']'", globPattern, i - 1);
+    }
+    regex.append("]]");
+    return i;
+  }
+  private static int handleInitialCharacters(String globPattern, StringBuilder regex, int i) {
+    if (next(globPattern, i) == '^') {
+      regex.append("\\^");
+      i++;
+    } else {
+      if (next(globPattern, i) == '!') {
+        regex.append('^');
+        i++;
+      }
+      if (next(globPattern, i) == '-') {
+        regex.append('-');
+        i++;
+      }
+    }
+    return i;
+  }
+
+  @SuppressWarnings("java:S3516")
+  private static int handleRange(String globPattern, StringBuilder regex, int i, boolean hasRangeStart, char last) {
+    if (!hasRangeStart) {
+      throw new PatternSyntaxException("Invalid range", globPattern, i - 1);
+    }
+    char c = next(globPattern, i++);
+    if (c == EOL || c == ']') {
+      return i;
+    }
+    if (c < last) {
+      throw new PatternSyntaxException("Invalid range", globPattern, i - 3);
+    }
+    regex.append(c);
+    return i;
   }
 
   private static char next(String glob, int i) {
